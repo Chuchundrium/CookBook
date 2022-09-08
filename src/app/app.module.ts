3ba050b97abd429c62b43f630dc8d23ea49da413
Chuchundrium <aptolya@gmail.com>
2022-09-08 18:47:31 +0300

    Minor update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppBodyComponent } from './app-body/app-body.component';
+import { NavigationComponent } from './app-body/navigation/navigation.component';
+import { IngredientsComponent } from './app-body/ingredients/ingredients.component';
+import { DescriptionComponent } from './app-body/description/description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppBodyComponent,
+    NavigationComponent,
+    IngredientsComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule
